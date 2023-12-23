01dac4eea17a25f0ffea076d622ed44f286ba578
patilpriyanka2325 <patilpriyanka2325@gmail.com>
2023-12-23 12:01:00 +0530

    Add project files.

---
+++ Delegate1/Employee.cs
@@ -0,0 +1,26 @@
+﻿public class Employee(int employeeID, string name, int salary, int experiance) // parametrized constructor
+{
+    public int EmployeeID { get; set; } = employeeID;
+    public string Name { get; set; } = name;
+    public int Salary { get; set; } = salary;
+    public int Experiance { get; set; } = experiance;
+
+    public static void PromoteEmployee(List<Employee> employees, IsPromotable isEligibleToPromote)
+    {
+        foreach (var employee in employees)
+        {
+            if (isEligibleToPromote(employee))
+            {
+                Console.WriteLine(employee.Name + " promoted");
+            }
+        }
+    }
+
+    public static bool Promote(Employee employee)
+    {
+        if (employee.Experiance >= 5)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ Delegate1/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Employee> employees =
+        [
+            new Employee(101, "Priyanka", 10000, 10),
+            new Employee(102, "Suraj", 80000, 8),
+            new Employee(103, "Vishu", 11000, 11),
+            new Employee(104, "Rohit", 4000, 4),
+            new Employee(101, "Rahi", 3000, 3),
+        ];
+
+        //IsPromotable isPromotable = new(Employee.Promote);
+        //Employee.PromoteEmployee(employees, isPromotable);
+
+        // Simply using LINQ ... No need of Employee.Promote()
+        Employee.PromoteEmployee(employees, employee => employee.Experiance >= 5);
+    }
+}
+
+public delegate bool IsPromotable(Employee employee);
